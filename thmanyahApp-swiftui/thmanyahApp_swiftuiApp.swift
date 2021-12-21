e5975a069cc7c3dcd2cf38547deeeed80f1ba703
vdotup <v.up@live.com>
2021-12-21 12:55:41 +0300

    Initial Commit

---
+++ thmanyahApp-swiftui/thmanyahApp_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  thmanyahApp_swiftuiApp.swift
+//  thmanyahApp-swiftui
+//
+//  Created by Abdurrahman Alfudeghi on 21/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct thmanyahApp_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
